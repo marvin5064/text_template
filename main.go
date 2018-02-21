625c2d3e8c46e382bef0110b98c5ddda80c11d8c
marvin5064 <marvin5064@gmail.com>
2018-02-21 12:19:56 +0800

    Test squash (#1)
    * init log
    
    * finished log

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ type Data struct {
 }
 
 func main() {
+	fmt.Println("Begin:")
+	defer func() {
+		fmt.Println("Done.")
+	}()
 	templateSample := `
 	current_total_array_number: {{ len (.) }}
 	{{ $total_number := len (.)}}
